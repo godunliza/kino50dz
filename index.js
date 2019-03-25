66786b1fa10be08ba7c14d2f1c1de1dd38467ef6
Your Name <you@example.com>
2019-03-25 19:05:52 +0200

    change

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
+
 /*Получение данных */
 var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/photos', false);
-console.log("Zavhlo!")
+
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/photos',true);//заменить на ассинхронный
+xhr.responseType = "json"; 
 xhr.send();
 
-if (xhr.status != 200) {
-  
- alert( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
-} else {
-  var par=JSON.parse(xhr.responseText);
+var resp;
+xhr.onload=function(){
+  resp=xhr.response;
 
-  var titles= par.map(function(num) {
-    return num.title;
-    
-  });
-  var url= par.map(function(num) {
-    return num.url;
-    
-  });
-  var thumbnailUrl= par.map(function(num) {
-    return num.thumbnailUrl;
-    
-  });
+var titles=[],
+    url_big=[],
+    url_sm=[];
 
-}
+  resp.forEach(
+    function(element,index){
+      titles[index]=element.title;
+      url_big[index]=element.url;
+      url_sm[index]=element.thumbnailUrl;
+    }
+  )
+
+
+//}
 /**************************************************************/
 
 if ('content' in document.createElement('template')) {
@@ -33,20 +33,31 @@ var items = document.querySelector('#items');//destination
 var fragment = document.createDocumentFragment();//fragment
 
 
-var all= item.content.querySelectorAll("a,img,h4,ul");
-for(let i=0;i<=49;i++)
-{
-all[0].href=url[i];
-all[1].src=thumbnailUrl[i];
-all[1].alt="Film picture";
-all[2].innerHTML=titles[i]
-var clone = document.importNode(item.content, true);
-fragment.appendChild(clone);
+
+var a=item.content.querySelectorAll("a");
+var img=item.content.querySelectorAll("img");
+var zag=item.content.querySelectorAll("h4");
+var stars=item.content.querySelectorAll("ul");
+
+
+console.log("a",typeof(img[0]));
 
 }
+
+titles.forEach(
+  function(element,index){
+    if(index<50)
+    {
+    element.href=url_big[index];
+    img[0].src=url_sm[index];
+    img[0].alt="Film picture";
+    zag[0].innerHTML=titles[index];
+    var clone = document.importNode(item.content, true);
+    fragment.appendChild(clone);}
+  
+  }
+)
+
 items.appendChild(fragment);
 
-} else {
-  console.log("template Не поддерживается!")
-  
 }
