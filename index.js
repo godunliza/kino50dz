f3cf0011cc30e5f0b28ca83899fd7e6831cff5ae
Your Name <you@example.com>
2019-03-31 13:59:47 +0300

    change3103

--- index.js
+++ index.js
@@ -1,63 +1,45 @@
-
 /*Получение данных */
 var xhr = new XMLHttpRequest();
-
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/photos',true);//заменить на ассинхронный
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/photos',true);
 xhr.responseType = "json"; 
-xhr.send();
 
+var MAX_IMAGES=50;//Максимальное количество карточек фильмов
 var resp;
+
 xhr.onload=function(){
   resp=xhr.response;
-
-var titles=[],
-    url_big=[],
-    url_sm=[];
-
-  resp.forEach(
-    function(element,index){
-      titles[index]=element.title;
-      url_big[index]=element.url;
-      url_sm[index]=element.thumbnailUrl;
-    }
-  )
-
-
-//}
-/**************************************************************/
-
-if ('content' in document.createElement('template')) {
+    if ('content' in document.createElement('template')) {
  
-var item = document.querySelector('#itemsh');//template
-var items = document.querySelector('#items');//destination
-var fragment = document.createDocumentFragment();//fragment
+        var item = document.querySelector('#itemsh');//template
+        var items = document.querySelector('#items');//destination
+        var fragment = document.createDocumentFragment();//fragment
 
+        var a=item.content.querySelector("a");
+        var img=item.content.querySelector("img");
+        var zag=item.content.querySelector("h4");
+        var stars=item.content.querySelector("ul");
 
+        stars.classList="mark";
+        stars.children.classList="star";
 
-var a=item.content.querySelectorAll("a");
-var img=item.content.querySelectorAll("img");
-var zag=item.content.querySelectorAll("h4");
-var stars=item.content.querySelectorAll("ul");
-
-
-console.log("a",typeof(img[0]));
-
-}
-
-titles.forEach(
-  function(element,index){
-    if(index<50)
-    {
-    element.href=url_big[index];
-    img[0].src=url_sm[index];
-    img[0].alt="Film picture";
-    zag[0].innerHTML=titles[index];
-    var clone = document.importNode(item.content, true);
-    fragment.appendChild(clone);}
-  
-  }
-)
+        /****************************************/
+        resp.slice(0, MAX_IMAGES).forEach(
+        function(element,index){
+            
+            
+            element.href=element.url;
+            img.src=element.thumbnailUrl;;
+            img.alt="Film picture";
+            zag.innerHTML=element.title;
+            var clone = document.importNode(item.content, true);
+            fragment.appendChild(clone);}
+        
+        
+        )
 
 items.appendChild(fragment);
 
 }
+
+}
+xhr.send();
